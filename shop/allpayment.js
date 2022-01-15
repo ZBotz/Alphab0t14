fac2d0f9acff32b21035ef4646e35c93d8735d4b
ZBotz <97812181+ZBotz@users.noreply.github.com>
2022-01-16 06:21:05 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,28 +1,16 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY フィノマヘサ
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 0858 9578 9545
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0858 9578 9545
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0858 9578 9545
 	
-Semuanya atas nama RI****DI
-
+Semuanya atas nama FI****SA
 *📲 PULSA*
 
 Tidak tersedia
